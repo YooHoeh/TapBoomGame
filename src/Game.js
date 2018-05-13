22d7a914fd5904b9f4d8a325d3948b46d7d68125
YooHoeh <zako2810@sina.com>
2018-05-13 18:29:28 +0800

    init

---
+++ src/Game.js
@@ -0,0 +1,23 @@
+import {
+    React
+} from 'react';
+import {
+    View,
+
+
+} from 'react-native';
+
+class CountNum extends Componet {
+    state = {
+        count: this.props.time,
+    };
+    render(){
+        const
+    }
+}
+export default class Game extends Componet {
+    constructor (props) {
+        super(props);
+    }
+    state
+}
