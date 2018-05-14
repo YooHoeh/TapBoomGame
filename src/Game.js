9766617ab0a552802c4dec4003b8ac1d2f387f09
YooHoeh <zako2810@sina.com>
2018-05-14 08:46:23 +0800

    init

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import {
 } from 'react';
 import {
     View,
-
+    Text,
 
 } from 'react-native';
 
@@ -11,13 +11,31 @@ class CountNum extends Componet {
     state = {
         count: this.props.time,
     };
-    render(){
-        const
+
+    render() {
+        const {count} = this.state;
+        return (
+            <View>{count > 0 && <Text>{count}</Text>}</View>
+        );
+    }
+    componentDidMount(){
+        this.timer = setInterval(()=>{
+            const { count } = this.state;
+            if (count === 0) return clearInterval(this.timer);
+            this.setState({
+                count: count -1,
+            })
+        },1000);
+    }
+    componetWillUnmount(){
+        clearInterval(this.timer);
     }
 }
+
 export default class Game extends Componet {
-    constructor (props) {
+    constructor(props) {
         super(props);
     }
+
     state
 }
